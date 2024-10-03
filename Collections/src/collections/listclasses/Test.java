7c6b02d0c4b2ad4fdb2c2a2f30b6438bfb2972db
riteshgaigawali <riteshgaigawali2001@gmail.com>
2024-10-03 18:05:55 +0530

    Eclipse setup done.

---
+++ Collections/src/collections/listclasses/Test.java
@@ -0,0 +1,15 @@
+package collections.listclasses;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Start @Cyber Success");
+		
+		System.out.println("Welcome to the Test class.");
+		
+		System.out.println("Stop @Cyber Success");
+
+	}
+
+}
